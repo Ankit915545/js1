2801d66abc49b98b8df0359756c12989f9eb6e76
Ankit915545 <ankiotkumarsharma1996@gmail.com>
2022-10-04 15:02:52 +0530

    simple commit

---
+++ sceipt.js
@@ -0,0 +1,32 @@
+/*
+var price1=50;
+var price2=100;
+var total=price1+price2;
+console.log(total)
+*/
+
+let a=50
+let b=68
+let total=a+b;
+let c=a*b;
+let c1=a/b;
+let c2=a%b
+let c3=++a;
+let c4=b++;
+let c5= (a==b);
+let c6= a===b;
+var c7=a!=b;
+var c8=a>b;
+const c9=a<b;
+const c10=a>=b;
+var d="js"+total
+var d1="2"+4+4
+let d3=a+=b;
+let d4=a-=b
+var d5=a*=b;
+var d6=a/=b;
+var d7=a%=b;
+
+console.log(total,c,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,d,d1,d3,d4,d5,d6,d7)
+
+
